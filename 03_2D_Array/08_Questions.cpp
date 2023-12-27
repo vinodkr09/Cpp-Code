6515500e0c7aaed163e36a155877bd7a79308a15
vinodkr09 <vinod.krmahi@gmail.com>
2023-12-28 00:56:26 +0530

    questions added

---
+++ 03_2D_Array/08_Questions.cpp
@@ -0,0 +1,11 @@
+// Problem: Print Like a Wave
+link-> https://www.codingninjas.com/studio/problems/print-like-a-wave_893268?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_6&leftPanelTabValue=SUBMISSION
+
+// Sprial print
+Approach:(1) print starting row
+         (2) print ending column
+         (3) print print ending row
+         (4) pritnt starting column
+link-> https://leetcode.com/problems/spiral-matrix/submissions/1123158748/
+
+//Binary Search 2-D Matrix
