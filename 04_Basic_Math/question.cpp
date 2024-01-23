6da9d625a33d49a1bd42d5dda996a2986015f32f
vinodkr09 <vinod.krmahi@gmail.com>
2024-01-24 02:04:15 +0530

    question

---
+++ 04_Basic_Math/question.cpp
@@ -0,0 +1,2 @@
+// Modular Exponentiation
+link ->> https://www.codingninjas.com/studio/problems/modular-exponentiation_1082146?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_7&leftPanelTabValue=PROBLEM
