45af37d640bda68ce814d08859e7d92a500c10ac
vinodkr09 <vinod.krmahi@gmail.com>
2024-02-01 15:39:15 +0530

    pointer array

---
+++ 05_Pointer/04_pointer_array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10] = {2, 5, 6};
+    cout<< "address of first memory block is " << arr << endl;  // gives output as 1st location address
+    cout<< arr[0]<< endl;  // gives output value at zero index i.e 2
+    cout<< "address of first memory block is " << &arr[0] <<endl;   // gives output as 1st location address
+    cout << "4th :" << *arr <<endl;  // output is 2
+    cout << "5th : " << *arr + 1 << endl;  // output is 3
+    cout << "6th : " << *(arr + 1) << endl;  //output is 5
+    cout << "7th : " << *(arr) + 1 << endl;  // output is 3
+    cout << "8th : " << arr[2]<< endl;    // output is 6
+    cout << "9th : " << *(arr + 2) << endl;   // output is 6
+    
+
+
+
+    return 0;
+
+
+}
