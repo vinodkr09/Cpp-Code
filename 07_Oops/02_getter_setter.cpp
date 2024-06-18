63288b0a7a4ea551b5e2c2412a12a2f751449502
vinodkr09 <vinod.krmahi@gmail.com>
2024-06-18 17:37:57 +0530

    getter and setter

---
+++ 07_Oops/02_getter_setter.cpp
@@ -0,0 +1,50 @@
+// To access private data memebers we can use get() and set() function
+#include<iostream>
+using namespace std;
+
+class Hero {
+private:
+   int health;
+
+public:
+    char level;
+
+    void print(){
+        cout<< level << endl;
+    }
+    
+    int getHealth(){
+        return health;
+    }
+
+    char getLevel(){
+        return level;
+    }
+
+    void setHealth(int h){
+        health = h;
+    }
+
+   void setLevel(char ch){
+    level = ch;
+   }
+    
+};
+
+
+int main(){
+    // creation of object 
+    Hero ramesh;
+
+    cout<<"Ramesh health is: "<< ramesh.getHealth()<<endl;
+
+    //use setter
+    ramesh.setHealth(70);
+    ramesh.level = 'A';
+
+    cout<<"health is: " << ramesh.getHealth() << endl;
+    cout << "Level is: " << ramesh.level << endl;
+
+    return 0;
+
+}
